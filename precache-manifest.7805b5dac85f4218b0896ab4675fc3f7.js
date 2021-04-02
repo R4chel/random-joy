cea658f0c58e27204ab995e6eaf4aa9ab455dff6
R4chel <rae84@cornell.edu>
2021-04-02 16:30:04 -0400

    Updates

---
+++ precache-manifest.7805b5dac85f4218b0896ab4675fc3f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/random-joy/favicon.ico"
+  },
+  {
+    "revision": "980b3f9c88b5a9c2a14a69eb5fae7e3f",
+    "url": "/random-joy/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/random-joy/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/random-joy/manifest.json"
+  },
+  {
+    "revision": "9937ac400b624fb55753",
+    "url": "/random-joy/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "9937ac400b624fb55753",
+    "url": "/random-joy/static/js/main.9937ac40.chunk.js"
+  },
+  {
+    "revision": "f9ff1b40944cb7cd7426",
+    "url": "/random-joy/static/js/runtime~main.f9ff1b40.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/random-joy/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
